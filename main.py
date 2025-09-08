ec6e916e7a0a651f91a14d93855b8341e0bacf45
Ramyashree034 <151832023+Ramyashree034@users.noreply.github.com>
2025-09-08 22:48:56 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+from typing import List
+from fastapi import FastAPI
+from pydantic import BaseModel
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
+
+app = FastAPI(title="String to Characters")
+
+class TextIn(BaseModel):
+    text: str
+
+@app.post("/chars", response_model=List[str])
+def string_to_chars(payload: TextIn) -> List[str]:
+    s = payload.text
+    if len(s.strip()) == 0:
+        return ["INVALID STRING"]
+    return list(s)
+
+# Serve frontend
+app.mount("/static", StaticFiles(directory="."), name="static")
+
+@app.get("/")
+def home():
+    return FileResponse("index.html")
